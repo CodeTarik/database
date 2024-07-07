9ecf2e9bac34b9b03c5885399689ac491c955205
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-07 12:19:25 +0200

    commit fifht

--- datenbank.js
+++ datenbank.js
@@ -1,24 +1,26 @@
 let names = [];
+
 const BASE_URL = "https://remotestorage-3f179-default-rtdb.europe-west1.firebasedatabase.app/";
 
 async function onloadFunc(){
     let userResponse = await getAllUsers("namen");
-    let UserKeysArray = Object.keys(userResponse);
-
-    console.log(UserKeysArray);
+    console.log(userResponse);
 
     await addEditSingleUser();
 }
 
-async function putData(path="", data={}) { 
-}
+async function putData(path = "", data = {}){ 
 
-async function addEditSingleUser(id="11", user={name: 'Tarik'}){
-    putData(`name/${id}`, user);
 }
 
 
+async function addEditSingleUser(id=11, user={name: 'Tarik'}){
+    putData(`namen/${id}`, user);
+}
 async function getAllUsers(path) {
     let response = await fetch(BASE_URL + path + ".json");
     return responseToJson = await response.json();
 }
+
+onloadFunc();
+
