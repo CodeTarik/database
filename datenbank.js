56be05e79e6a926d12d3485cabed9850141321ae
Tarik Bozüslü <boz.tarik38@gmail.com>
2024-07-07 12:28:31 +0200

    putData

--- datenbank.js
+++ datenbank.js
@@ -10,17 +10,35 @@ async function onloadFunc(){
 }
 
 async function putData(path = "", data = {}){ 
+    try {
+        let response = await fetch(BASE_URL + path + ".json", {
+            method: "PUT", // Methode auf PUT setzen
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data) // Daten in JSON-String umwandeln
+        });
 
+        if (!response.ok) {
+            throw new Error("Network response was not ok: " + response.statusText);
+        }
+
+        let responseData = await response.json();
+        console.log("Data successfully uploaded:", responseData);
+        return responseData;
+        } catch (error) {
+        console.error("Error in putData:", error);
+        return null;
+    }
 }
 
-
-async function addEditSingleUser(id=11, user={name: 'Tarik'}){
-    putData(`namen/${id}`, user);
+async function addEditSingleUser(id = 33, user = {name: 'Haci Yusuf', email: 'mustermann@gmail.com'}) {
+    await putData(`namen/${id}`, user);
 }
+
 async function getAllUsers(path) {
     let response = await fetch(BASE_URL + path + ".json");
-    return responseToJson = await response.json();
+    return await response.json();
 }
 
 onloadFunc();
-
